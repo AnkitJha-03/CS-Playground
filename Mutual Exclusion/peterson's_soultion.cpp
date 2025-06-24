d11366555a0948b123d6057f6e396c78ea142a09
Ankit Jha <ankitjhax03@gmail.com>
2025-06-25 00:15:44 +0530

    peterson's-solution

---
+++ Mutual Exclusion/peterson's_soultion.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <thread>
+
+int turn = 0;
+bool interested[2];
+int x = 0;
+
+void enter_section(int process){
+  int other = 1 - process;
+  interested[process] = true;
+  turn = process;
+  // if its my turn, I will wait until the other process is not interested
+  while(turn == process && interested[other] == true);
+}
+
+void leave_section(int process){
+  interested[process] = false;
+}
+
+class thread_object {
+public:
+  void operator()(int process) {
+    for (int i = 0; i < 100; i++) {
+      enter_section(process);
+      x += 1;
+      std::cout<<"Thread "<<process<<" "<<x<<std::endl;
+      leave_section(process);
+    }
+  }
+};
+
+int main(){
+  thread_object obj;
+  std::thread th1(obj, 0);
+  std::thread th2(obj, 1);
+
+  th1.join();
+  th2.join();
+
+  return 0;
+}
--- Mutual Exclusion/wait_turn.cpp
+++ Mutual Exclusion/wait_turn.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
 #include <thread>
+#include <windows.h>
 
 int turn = 0;
 
 void process0() {
-    while (true) {
-        while (turn != 0); // Busy-wait (spin), until it's Process 0's turn
+  while (true) {
+    while (turn != 0); // Busy-wait (spin), until it's Process 0's turn
 
-        // Critical section for Process 0
-        std::cout << "Process 0 in critical section" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        turn = 1; // Release lock (give turn to Process 1)
+    // Critical section for Process 0
+    std::cout << "Process 0 in critical section" << std::endl;
+    Sleep(500);
+    turn = 1; // Release lock (give turn to Process 1)
 
-        // non-critical section for Process 0
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
+    // non-critical section for Process 0
+    Sleep(500);
+  }
 }
 
 void process1() {
-    while (true) {
-        while (turn != 1); // Busy-wait (spin), until it's Process 1's turn
+  while (true) {
+    while (turn != 1); // Busy-wait (spin), until it's Process 1's turn
 
-        // Critical section for Process 1
-        std::cout << "Process 1 in critical section" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        turn = 0; // Release lock (give turn to Process 0)
+    // Critical section for Process 1
+    std::cout << "Process 1 in critical section" << std::endl;
+    Sleep(500);
+    turn = 0; // Release lock (give turn to Process 0)
 
-        // non-critical section for Process 1
-        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    }
+    // non-critical section for Process 1
+    Sleep(5000);
+  }
 }
 
 int main() {
-    std::thread t0(process0);
-    std::thread t1(process1);
+  std::thread t0(process0);
+  std::thread t1(process1);
 
-    t0.join();
-    t1.join();
+  t0.join();
+  t1.join();
 
-    return 0;
+  return 0;
 }
