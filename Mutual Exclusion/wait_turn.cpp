b792b807ddd4f7959d05d0028277da421473fd03
Ankit Jha <ankitjhax03@gmail.com>
2025-06-24 23:48:26 +0530

    wait-turn

---
+++ Mutual Exclusion/wait_turn.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+
+int turn = 0;
+
+void process0() {
+    while (true) {
+        while (turn != 0); // Busy-wait (spin), until it's Process 0's turn
+
+        // Critical section for Process 0
+        std::cout << "Process 0 in critical section" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        turn = 1; // Release lock (give turn to Process 1)
+
+        // non-critical section for Process 0
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+void process1() {
+    while (true) {
+        while (turn != 1); // Busy-wait (spin), until it's Process 1's turn
+
+        // Critical section for Process 1
+        std::cout << "Process 1 in critical section" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        turn = 0; // Release lock (give turn to Process 0)
+
+        // non-critical section for Process 1
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    }
+}
+
+int main() {
+    std::thread t0(process0);
+    std::thread t1(process1);
+
+    t0.join();
+    t1.join();
+
+    return 0;
+}
